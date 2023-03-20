6bcaa9723438333b8f3eb5b419c67aca0b3b4416
ASUS1 <paddyoop@gmail.com>
2023-03-20 13:12:51 +0700

    innit

---
+++ app/src/main/java/com/anmp/adv160420121week4/StudentListFragment.kt
@@ -0,0 +1,26 @@
+package com.anmp.adv160420121week4
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StudentListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StudentListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_list, container, false)
+    }
+
+
+}
